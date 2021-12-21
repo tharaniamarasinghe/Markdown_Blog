99705b1bc97a14175582054e79d91eb73a99328e
Damith <37-se-0005@kdu.ac.lk>
2021-12-21 17:23:05 +0530

    Display the article details

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req,res) => {
-    res.send('In articles')
+router.get('/new', (req,res) => {
+    res.render('articles/new')
+})
+
+router.post('/new', (req,res) => {  //handle post request
+    
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -4,14 +4,24 @@ const router = express.Router()
 
 const app = express()
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') //convert the ejs code in to html
 
 app.use('/articles', articleRouter)
 
 // respond with "Hi" when a GET request is made to the homepage
 app.get('/', (req,res) => {
     //res.send('Hello')
-    res.render('index') //render access views folder
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Test description'
+    },
+    {
+        title: 'Test Article 2',
+        createdAt: new Date(),
+        description: 'Test description 2'
+    }]
+    res.render('articles/index', { articles : articles }) //render access views folder
 })
 
 app.listen(5000)
