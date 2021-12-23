8d93448537629ac1233c34fa994ede6b6414f5a5
tharaniamarasinghe <tharaniamarasinghe99@gmail.com>
2021-12-23 14:37:10 +0530

    New article

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new', (req,res) => {
-    res.render('articles/new')
+    res.render('articles/new', { article: new Article()})
 })
 
-router.post('/new', (req,res) => {  //handle post request
-    
+router.get('/:id', async (req,res) => {
+    const article = await Article.findById(req.params.id)
+    if (article == null) res.redirect('/')  //redirect to homepage
+    res.render('articles/show', {article: article})
+})
+
+router.post('/', async (req,res) => {  //handle post request
+    let article = new Article({   //creating new article
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    })
+    try{
+        article = await article.save() //saving article
+        res.redirect(`/articles/${article.id}`)
+    } 
+    catch (e){
+        res.render('articles/new', {article: article})
+    }
+
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const router = express.Router()
-
 const app = express()
+//var MongoClient = require('mongodb').MongoClient
+
+/*
+var url = "mongodb://localhost:27017/dtblog"
+
+MongoClient.connect(url, function(err,dtblog){
+    if (err) throw err
+    console.log("Database created")
+    db.close()
+})
+*/
+
+mongoose.connect('mongodb://127.0.0.1:27017/dtblog', {useNewUrlParser: true})
+
+//mongoose.connect('mongodb://localhost:27017/{DT_BLOG}')
+
+/*, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+*/
 
 app.set('view engine', 'ejs') //convert the ejs code in to html
+app.use(express.urlencoded({extended: false}))
 
-app.use('/articles', articleRouter)
-
-// respond with "Hi" when a GET request is made to the homepage
 app.get('/', (req,res) => {
-    //res.send('Hello')
+    //res.send('Hello') // respond with "Hi" when a GET request is made to the homepage
     const articles = [{
         title: 'Test Article',
         createdAt: new Date(),
@@ -23,5 +41,7 @@ app.get('/', (req,res) => {
     }]
     res.render('articles/index', { articles : articles }) //render access views folder
 })
+
+app.use('/articles', articleRouter)
 
 app.listen(5000)
